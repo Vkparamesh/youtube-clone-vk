b40087826e5f5440909b0de455d987921c9ec56a
Vkparamesh <parameshvk3232@gmail.com>
2022-11-24 20:31:44 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Box } from "@mui/material"
-import Navbar from "./components/Navbar"
-import Feed from "./components/Feed"
-import ChannelDetail from './components/ChannelDetail'
-import SearchFeed from "./components/SearchFeed"
-import VedioDetail from "./components/VedioDetail.jsx"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
+
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
 
 const App = () => (
-    <BrowserRouter>
-        <Box sx={{ backgroundColor: '#000' }}>
-            <Navbar />
-            <Routes>
-                <Route path='/' exact element={<Feed />} />
-                <Route path='/video/:id' element={<VedioDetail />} />
-                <Route path='/channel/:id' element={<ChannelDetail />} />
-                <Route path='/search/:searchTerm' element={<SearchFeed />} />
-            </Routes>
-        </Box>
-    </BrowserRouter>
-)
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
-export default App
+export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,42 +4,42 @@ import { Paper, IconButton } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 
 const SearchBar = () => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
 
-    const onhandleSubmit = (e) => {
-        e.preventDefault();
-                                                                                                                                                                                                                                                                                                    
-        if (searchTerm) {
-            navigate(`/search/${searchTerm}`);
+  const onhandleSubmit = (e) => {
+    e.preventDefault();
 
-            setSearchTerm('');
-        }
-    };
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
 
-    return (
-        <Paper
-            component='form'
-            onSubmit={onhandleSubmit}
-            sx={{
-                borderRadius: 20,
-                border: '1px solid #e3e3e3',
-                pl: 2,
-                boxShadow: 'none',
-                mr: { sm: 5 },
-            }}
-        >
-            <input
-                className='search-bar'
-                placeholder='Search...'
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-            />
-            <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
-                <SearchIcon />
-            </IconButton>
-        </Paper>
-    );
+      setSearchTerm('');
+    }
+  };
+
+  return (
+    <Paper
+      component='form'
+      onSubmit={onhandleSubmit}
+      sx={{
+        borderRadius: 20,
+        border: '1px solid #e3e3e3',
+        pl: 2,
+        boxShadow: 'none',
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className='search-bar'
+        placeholder='Search...'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
 };
 
-export default SearchBar; 
+export default SearchBar;
